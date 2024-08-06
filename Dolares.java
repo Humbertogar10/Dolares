644bfa21fe2747baea3b1ca9831d8eedc09fe48f
Humbertogar10 <jgarciag115.miumg.edu.gt>
2024-08-05 22:18:04 -0600

    Se agrega el proyecto

---
+++ Dolares.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Dolares{
+ public static void main(String args[]){
+  Scanner scanner = new Scanner(System.in);
+
+  //Leemos los Quetzales
+  System.out.print("Ingrese la cantidad de quetzales: ");
+  double Quetzales = scanner.nextDouble();
+
+   //Usamos la formula de convercion
+   double Dolares = Quetzales * 7.76;
+
+    //Mostramos las pulgadas de tela requeridas
+    System.out.println("La cantidad de dolares es: " + Dolares);
+   
+ }
+}
